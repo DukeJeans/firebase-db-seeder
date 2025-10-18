4d67281019c4c5fc1ba4ba851a1398da31e11801
Nicholas Jeans <nicholasjeans@gmail.com>
2025-10-18 16:49:22 -0600

    get app initialized

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const FIREBASE_CONFIG = require('./firebase-config.json');
+import { initializeApp } from 'firebase/app';
+import FIREBASE_CONFIG from './firebase-config.json' with { type: 'json' };
+
+const app = initializeApp(FIREBASE_CONFIG);
+
+console.log("Firebase app initialized successfully.");
